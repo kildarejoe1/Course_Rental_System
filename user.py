f0e0243c63122b295dbdb5300babef70601dfa5c
iiihenrymorrin <henry.morrin@iii.com>
2018-08-27 12:31:57 +0100

    Added a method to the User class definition - to remove movies from movies list of movie class objects

--- user.py
+++ user.py
@@ -10,3 +10,12 @@ class User:
     def watched_movies(self):
         movies_watched= list(filter(lambda movie: movie.watched, self.movies))
         return movies_watched
+
+    def add_movie(self,name,director, genre): #my_user_object
+        movie = Movie(name,genre, False)
+        self.movies.append(movie)
+
+    def remove_movie(self,movie_name):
+        for movie in self.movies:
+            if movie.name == movie_name:
+                self.movies.remove(movie)
